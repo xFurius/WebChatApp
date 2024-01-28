b8a19d0697840ec8493e4a3aa8c1f5748fb266d5
Mateusz Pszczoła <matip03211@gmail.com>
2024-01-28 23:17:47 +0100

    added user repository and data persistence

--- src/main/java/com/example/webChatApp/user/User.java
+++ src/main/java/com/example/webChatApp/user/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/webChatApp/user/UserController.java
+++ src/main/java/com/example/webChatApp/user/UserController.java
@@ -1,5 +1,6 @@
 package com.example.webChatApp.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +8,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/api/signUp")
     public @ResponseBody User signUpTest(@RequestBody User user) {
         System.out.println(user.toString());
-        //user binded
         //set uid
         //encode password and user.setpassword
-        //userrepository.save(user)
-
+        userRepository.save(user);
         return user;
     }  
 }
